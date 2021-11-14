65fe74afc20c0328044a5c4103fd66f094442b3c
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2021-11-14 14:23:27 +0100

    notebook(update): update

--- subsample.sh
+++ subsample.sh
@@ -6,7 +6,7 @@ for dir in $(ls data); do
     mkdir subset/$dir
     n=0
     # for f in $(ls -lh data/$dir | sed -n "s/.*8[0-9]K.* \(.*-$n.\?\.png$\)/\1/p" | tail -n $1); do
-    for f in $(ls data/$dir | sed -n "s/\(.*-32[IGH]\?\.png$\)/\1/p" | tail -n $1); do
+    for f in $(ls data/$dir | sed -n "s/\(.*-31H\.png$\)/\1/p" | tail -n $1); do
         cp data/$dir/"$f" subset/$dir/$n.png
         n=$((n+1))
     done
